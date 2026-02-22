9805d7f51b39923e4fb02826000e8dbedb120326
Saht05 <nnlf0976@leeds.ac.uk>
2026-02-22 20:13:39 +0000

    df

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -10,15 +10,19 @@
  int main( void ) {
 
     // define and initialise variables for the problem data 
-   float salary=36250.0;
-   float NI=8.0;
-   float tax=15.0;
-
+   float salary=30000.0;
+   float NI=9.0;
+   float tax=25.0;
+   float tax_con;
 
     // calculate the deductions and final take-home salary
    float ni_con= salary* (NI/100.0);
    float sal_ni=salary-ni_con;
-   float tax_con= sal_ni*(tax/100.0);
+   if (sal_ni>12500){
+      tax_con= (sal_ni-12500)*(tax/100.0);
+   } else{
+      tax_con=0.0;
+   }
    float take_home= sal_ni-tax_con;
 
 
